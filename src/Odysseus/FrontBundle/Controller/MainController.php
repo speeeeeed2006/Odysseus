da63bfe004cb1657015b3b848ec5da0b35d2710b
Caroline <carolinebleier@gmail.com>
2015-02-26 11:27:57 +0100

    Modif des templates, ajout du slider, menu dynamique

--- src/Odysseus/FrontBundle/Controller/MainController.php
+++ src/Odysseus/FrontBundle/Controller/MainController.php
@@ -6,6 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller
 {
+    
+    public function menuAction()
+    {   
+        $repository = $this->getDoctrine()->getManager()->getRepository('OdysseusFrontBundle:Categorie');
+        $categorie = $repository->findAll();  
+        
+        return $this->render('OdysseusFrontBundle:Default:menu.html.twig', array('liste_categories' => $categorie));
+    }
+    
+    
+    
     public function homeAction()
     {
         return $this->render('OdysseusFrontBundle:Main:home.html.twig');
---
+++ web/js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('#coin-slider').coinslider({ width: 970, height:380, delay: 5000, effect:'straight' });
+    
+});
