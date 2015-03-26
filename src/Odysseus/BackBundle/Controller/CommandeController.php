8842bb8a2ba95118842a2fec9c3f9aa9aec3052b
Caroline <carolinebleier@gmail.com>
2015-03-26 22:15:37 +0100

    Back office : commadne suite

--- src/Odysseus/BackBundle/Controller/CommandeController.php
+++ src/Odysseus/BackBundle/Controller/CommandeController.php
@@ -21,55 +21,114 @@ class CommandeController extends Controller
         );
     }
     
-    /* A FAIRE
-     * public function commandeAEtePayee()
+
+    public function commandeValiderPayeeAction($id)
     {
-        $listeCommandes =  $this->getDoctrine()
-                                 ->getManager()
-                                 ->getRepository('OdysseusFrontBundle:Commande')
-                                 ->getCommandeEnAttentePaiement();
         
-        return $this->render('OdysseusBackBundle:Commande:listerCommandePayee.html.twig',
-        	array('liste_commandes' => $listeCommandes)
-        );
-    }*/
+        $em = $this->getDoctrine()->getManager();
+        
+        $commande = $em->getRepository('OdysseusFrontBundle:Commande')
+                      ->find($id);
+        
+        if (!$commande) {
+            throw $this->createNotFoundException('Aucun produit trouvé pour cet id : '.$id);
+        }
+        
+        $etat = $em->getRepository('OdysseusFrontBundle:Etat');
+        
+        $commande->setEtat($etat->find('10'));
+        $em->flush();
+        
+        //on affiche un message flash
+        $this->get('session')->getFlashBag()->add('commande', 'La commande est passée "payée"');
+
+        return $this->redirect($this->generateUrl('odysseus_back_lister_commande_payee'/*, array('page' => 1)*/));       
+    }
     
-    public function commandePayee()
+
+    
+    public function commandePayeeAction()
     {
         $listeCommandes =  $this->getDoctrine()
                                  ->getManager()
                                  ->getRepository('OdysseusFrontBundle:Commande')
                                  ->getCommandePayee();
         
-        $this->get('ladybug')->log($listeCommandes);
+        //$this->get('ladybug')->log($listeCommandes);
         
         return $this->render('OdysseusBackBundle:Commande:listerCommandePayee.html.twig',
         	array('liste_commandes' => $listeCommandes)
         );
     }
     
-    public function commandeEnCoursDeLivraison()
+    public function commandeValiderEnCoursLivraisonAction($id)
+    {
+        
+        $em = $this->getDoctrine()->getManager();
+        
+        $commande = $em->getRepository('OdysseusFrontBundle:Commande')
+                      ->find($id);
+        
+        if (!$commande) {
+            throw $this->createNotFoundException('Aucun produit trouvé pour cet id : '.$id);
+        }
+        
+        $etat = $em->getRepository('OdysseusFrontBundle:Etat');
+        
+        $commande->setEtat($etat->find('11'));
+        $em->flush();
+        
+        //on affiche un message flash
+        $this->get('session')->getFlashBag()->add('commande', 'La commande est passée "en cours de livraison"');
+
+        return $this->redirect($this->generateUrl('odysseus_back_lister_commande_en_cours_livraison'/*, array('page' => 1)*/));       
+    }
+    
+    public function commandeEnCoursDeLivraisonAction()
     {
         $listeCommandes =  $this->getDoctrine()
                                  ->getManager()
                                  ->getRepository('OdysseusFrontBundle:Commande')
                                  ->getCommandeEnCoursDeLivraison();
         
-        $this->get('ladybug')->log($listeCommandes);
+        //$this->get('ladybug')->log($listeCommandes);
         
         return $this->render('OdysseusBackBundle:Commande:listerCommandeEnCoursDeLivraison.html.twig',
         	array('liste_commandes' => $listeCommandes)
         );
     }
     
-    public function commandeLivree()
+    public function commandeValiderLivreAction($id)
+    {
+        
+        $em = $this->getDoctrine()->getManager();
+        
+        $commande = $em->getRepository('OdysseusFrontBundle:Commande')
+                      ->find($id);
+        
+        if (!$commande) {
+            throw $this->createNotFoundException('Aucun produit trouvé pour cet id : '.$id);
+        }
+        
+        $etat = $em->getRepository('OdysseusFrontBundle:Etat');
+        
+        $commande->setEtat($etat->find('12'));
+        $em->flush();
+        
+        //on affiche un message flash
+        $this->get('session')->getFlashBag()->add('commande', 'La commande est passée "livrée"');
+
+        return $this->redirect($this->generateUrl('odysseus_back_lister_commande_livree'/*, array('page' => 1)*/));       
+    }
+    
+    public function commandeLivreeAction()
     {
         $listeCommandes =  $this->getDoctrine()
                                  ->getManager()
                                  ->getRepository('OdysseusFrontBundle:Commande')
                                  ->getCommandeLivree();
         
-        $this->get('ladybug')->log($listeCommandes);
+        //$this->get('ladybug')->log($listeCommandes);
         
         return $this->render('OdysseusBackBundle:Commande:listerCommandeLivree.html.twig',
         	array('liste_commandes' => $listeCommandes)
